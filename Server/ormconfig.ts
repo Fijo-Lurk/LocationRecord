56a655d9bb8c0317c7463277599ccc3b36b7429d
fjohansson <fjohansson@smithmicro.com>
2022-03-02 14:16:58 +0100

    server/feature - add sqlite DB and typeOrm

---
+++ Server/ormconfig.ts
@@ -0,0 +1,16 @@
+import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
+
+const config: SqliteConnectionOptions = {
+  type: 'sqlite',
+  database: 'locationDB',
+  entities: ['dist/src/**/*.entity.js'],
+  synchronize: true, // change to false when production migrations is in use
+
+  // This is for production migrations
+  // migrations: ['dist/src/db/migrations/*.js'],
+  // cli: {
+  //   migrationsDir: 'src/db/migrations',
+  // },
+};
+
+export default config;
