c42317c27af011291688cb81c5b7ae263b5d7a5b
fjohansson <fjohansson@smithmicro.com>
2022-03-18 16:38:57 +0100

    frontend/feature add service

--- Server/src/app.module.ts
+++ Server/src/app.module.ts
@@ -5,11 +5,19 @@ import { AppService } from './app.service';
 import { LocationModule } from './location/location.module';
 import config from 'ormconfig';
 import { Location } from './location/entities/location.entity';
+import { ConfigModule } from '@nestjs/config';
+import configuration from 'config/configuration';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(config),
     LocationModule,
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/config/env/${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+      load: [configuration],
+    }),
+
     /*     TypeOrmModule.forFeature([Location]), */
   ],
   controllers: [AppController],
