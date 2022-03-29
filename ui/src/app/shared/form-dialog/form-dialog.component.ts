1f812b469da71b900c2dd1598a1cb2c015161d37
fjohansson <fjohansson@smithmicro.com>
2022-03-29 16:02:16 +0200

    frontend/bug fix environment select empty value

--- ui/src/app/shared/form-dialog/form-dialog.component.ts
+++ ui/src/app/shared/form-dialog/form-dialog.component.ts
@@ -10,7 +10,9 @@ import {
 } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { LocationService } from '../service/location.service';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
   selector: 'app-form-dialog',
   templateUrl: './form-dialog.component.html',
@@ -35,15 +37,24 @@ export class FormDialogComponent implements OnInit {
   });
   constructor(
     @Inject(MAT_DIALOG_DATA)
-    public data: LocationData,
+    public data: {
+      customer_id: string;
+      environment: string;
+      app_id: string;
+      studio_url: string;
+    },
     public translateService: TranslateService,
     private locationService: LocationService
   ) {}
 
   ngOnInit(): void {}
 
-  onSubmit(form?: FormGroupDirective) {
+  onSubmit(form: FormGroupDirective) {
     form.value.environment = form.value.environment.toLowerCase();
-    this.locationService.update(this.data, form.value).subscribe();
+    this.data.environment = this.data.environment.toLowerCase();
+    this.locationService
+      .update(this.data, form.value)
+      .pipe(untilDestroyed(this))
+      .subscribe();
   }
 }
